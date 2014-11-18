ada16eecf45a716efda23451011313460c7358c9
Travis Himes <travis.himes@gmail.com>
2014-11-18 15:11:38 -0500

    added the screenplay changes this time

--- library/src/main/java/com/davidstemmer/screenplay/flow/Screenplay.java
+++ library/src/main/java/com/davidstemmer/screenplay/flow/Screenplay.java
@@ -57,8 +57,11 @@ public class Screenplay implements Flow.Listener {
             incomingScene.getTransformer().applyAnimations(sceneCut, this);
         }
         else if (outgoingScene != null) {
-            outgoingScene.getRigger().layoutIncoming(director.getContainer(), outgoingScene.getView(), direction);
+            outgoingScene.getRigger().layoutOutgoing(director.getContainer(), outgoingScene.getView(), direction);
             outgoingScene.getTransformer().applyAnimations(sceneCut, this);
+
+            incomingScene.getRigger().layoutIncoming(director.getContainer(), incomingScene.getView(), direction);
+            incomingScene.getTransformer().applyAnimations(sceneCut, this);
         }
         else {
             // backward, previous scene is null (?)
